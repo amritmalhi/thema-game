362aeadf15e6c63226396aa659438cde22668b17
Luguos <luguoslight@gmail.com>
2017-01-18 15:19:33 +0100

    Adjusted drawable.hpp

--- src/drawable.hpp
+++ src/drawable.hpp
@@ -1,3 +1,6 @@
+#ifndef _DRAWABLE_HPP
+#define _DRAWABLE_HPP
+
 #include <SFML\Graphics.hpp>
 
 class drawable {
@@ -7,3 +10,5 @@ public:
 	drawable(sf::Vector2f position);
 	virtual void draw(sf::RenderWindow & window) = 0;
 };
+
+#endif
