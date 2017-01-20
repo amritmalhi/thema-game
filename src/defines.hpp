a79ff776ce7033cc43f77080e39798c33faeafb1
Jeroen <Jeroen.kok@student.hu.nl>
2017-01-20 15:43:11 +0100

    Moved defines to a hpp special for defines

---
+++ src/defines.hpp
@@ -0,0 +1,6 @@
+#define RES_LOC "../"
+#define UPS 60
+#define UPS_MS std::chrono::milliseconds( 1000/UPS )
+#define FPS 60
+#define FPS_MS std::chrono::milliseconds( 1000/FPS )
+
--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,7 @@
 #include <chrono>
 #include "level.hpp"
 #include <iostream>
-
-#define RES_LOC "../"
-#define UPS 60
-#define UPS_MS std::chrono::milliseconds( 1000/UPS )
-#define FPS 60
-#define FPS_MS std::chrono::milliseconds( 1000/FPS )
+#include "defines.hpp"
 
 int main()
 {
